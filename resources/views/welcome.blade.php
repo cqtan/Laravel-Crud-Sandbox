245c3e0a503b35c118aaeb680ffad8fa8d5454f0
CouchCat <kalli.t@hotmail.com>
2016-10-28 19:59:43 +0200

    Try debug 01

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
+                    <a href="{{ url('/manage_content') }}">Login</a>
                     <a href="{{ url('/register') }}">Register</a>
                 </div>
             @endif
